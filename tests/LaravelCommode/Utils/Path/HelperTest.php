2705afef47fcb89e3c3eb5c7532d69dd5b77358a
dubpub <dubpubz@gmail.com>
2015-06-30 04:10:33 +0300

    fooley commit

--- tests/LaravelCommode/Utils/Path/HelperTest.php
+++ tests/LaravelCommode/Utils/Path/HelperTest.php
@@ -6,7 +6,11 @@ class HelperTest extends \PHPUnit_Framework_TestCase
 {
     public function testStripSlashes()
     {
-        $urls = ['/url', 'url/', '/url/'];
+        $urls = [
+            '/url',
+            'url/',
+            '/url/'
+        ];
 
         foreach ($urls as $url) {
             $this->assertSame('url', Helper::stripSlashes($url));
